a03ff057ffe4291c6947b790f82fd8be87d258f9
vedantg1000 <69249835+vedantg1000@users.noreply.github.com>
2021-08-17 19:23:30 +0530

    Add files via upload

---
+++ Day_1_Assignment/d1.py
@@ -0,0 +1,6 @@
+# Assignment Question:- Take two input and store in variable x and y,  find x to the power y.
+
+x = int(input("Enter base number: "))
+y = int(input("Enter index number: "))
+z = x**y
+print (z)
